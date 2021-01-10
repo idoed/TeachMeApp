ecb4a9d52d360920152ac8e53aa26b5745026f5d
barrubin <barrubin95@gmail.com>
2021-01-10 10:48:49 +0200

    bar_commit

---
+++ lib/profile.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+//import 'package:international_phone_input/international_phone_input.dart';
+
+
+
+class Profile extends StatelessWidget{
+
+
+  //  onValidPhoneNumber(
+  //     String number, String internationalizedPhoneNumber, String isoCode) {
+  //   setState(() {
+  //     confirmedNumber = internationalizedPhoneNumber;
+  //   });
+  // }
+
+  final userNameController = TextEditingController();
+  final phoneNumberController = TextEditingController();
+  final degreeNameController = TextEditingController();
+
+//   //select degree optional implement: map with degreeName key and list of names
+//   //final degree = {'degreeName':[]}; 
+//   //degree['degreeName'] will return this list
+
+ @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 50,0 ,50),
+          child: Stack(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 61,
+                child: ClipOval(child: Image.asset('https://www.pexels.com/photo/man-in-toga-holding-diploma-1007066/', height: 150, fit: BoxFit.cover,),),
+              ),
+              Positioned(bottom: 1, right: 1 ,child: Container(
+                height:40, width: 40,
+                child: Icon(Icons.add_a_photo, color: Colors.white,),
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.all(Radius.circular(20))
+                ),
+              ))
+            ],
+          ),
+        ),
+        Expanded(child: Container(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 25, 20, 4),
+                child: Container(
+                  height: 60,
+                  child:TextField(
+                      textAlign: TextAlign.right,
+                      textDirection: TextDirection.rtl,
+                      decoration: InputDecoration(
+                          labelText: 'שם מלא',
+                          icon: Icon(Icons.person ,color: Colors.blue)),
+               controller: userNameController,
+              ),
+                  decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(20)),border: Border.all(width: 1.0, color: Colors.blue)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 5, 20, 4),
+                child: Container(
+                  height: 60,
+                  child: TextField(
+                      textAlign: TextAlign.right,
+                      textDirection: TextDirection.rtl,
+                      decoration: InputDecoration(
+                          labelText: 'מספר פלאפון',
+                          icon: Icon(Icons.local_phone,color: Colors.blue)),
+                      controller: phoneNumberController,
+                ),
+                  decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(20)),border: Border.all(width: 1.0, color: Colors.blue)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 5, 20, 4),
+                child: Container(
+                  height: 60,
+                  child:TextField(
+                      textAlign: TextAlign.right,
+                      textDirection: TextDirection.rtl,
+                      decoration: InputDecoration(
+                          labelText: 'בחר/י תואר',
+                          icon: Icon(Icons.school,color: Colors.blue)),
+                      controller: degreeNameController,
+                ),
+                   decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(20)),border: Border.all(width: 1.0, color: Colors.blue)),
+                ),
+              ),
+            ],
+          ),
+        )),
+    //     Scaffold(     
+    //     body: Center(
+    //    child:  InternationalPhoneInputText(
+    //      onValidPhoneNumber: onValidPhoneNumber,
+    //     ),
+    //  ),
+    //  ),
+      ],
+    );
+    
+  }
+
+    }
+
+  
